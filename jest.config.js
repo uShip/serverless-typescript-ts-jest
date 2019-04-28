3a8e6c55b9a543875d4a672223e9ef5aa2a9cc38
Ben Yen <byen@uship.com>
2019-04-27 21:28:44 -0500

    Doing some stuff to fix test debugging

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,11 @@
 module.exports = {
+    testEnvironment: 'node',
+    transform: {
+        '^.+\\.(ts|tsx)$': 'ts-jest'
+    },
+    testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(tsx?)$',
+    testPathIgnorePatterns: ['/node_modules/', '.*\\.d\\.ts$'],
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
     roots: ['<rootDir>/src'],
     preset: 'ts-jest',
     testEnvironment: 'node',
